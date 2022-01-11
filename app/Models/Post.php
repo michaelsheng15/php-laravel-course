a47cda46b75a1f2ae64855fe90fe39ed168a4284
Michael Sheng <m3sheng@uwaterloo.ca>
2022-01-10 22:10:40 -0500

    Working through section 4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //3 ways to mitigate mass assignment vulnerbilities
+    //assignemtn refers to using Post::create([fields])
+
+    //1 - gaurding certain fields from assignment
+    // protected $guarded =['id']; 
+
+    //2 - being explicit about field that can be assigned
+    // protected $fillable = ['title', 'excerpt', 'body'];
+
+    //3 - turning off assignemnt completely
+    protected $guarded =[]; 
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
+
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
-    $post = Post::findOrFail($id);
-
+Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function(Category $category){
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
